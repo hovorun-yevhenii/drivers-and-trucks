e40ec7f558827554237a06c4bc49fe5538ea9403
oxygene4 <5797565@gmail.com>
2018-12-05 22:27:18 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">{{foo}}</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      foo: 'bar'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  color: #232323;
+}
+</style>
